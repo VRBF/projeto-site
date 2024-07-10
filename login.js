674240f238142606a67d778fa232cdf645f46b66
Maxuel <maxegi12@gmail.com>
2024-07-10 14:48:56 -0300

    Concluindo paginas de Login e cadastro

--- login.js
+++ login.js
@@ -10,8 +10,32 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         alert('Login realizado com sucesso!');
         window.location.href = 'pag inicial.html';
     } else {
-        alert('Email ou senha incorretos.');
+        alert('Email ou senha incorretos, por favor tente novamente ou crie uma conta');
     }
+});
+const clique = document.querySelector('#clique');
+const criarConta = document.querySelector('.criar-conta');
+criarConta.addEventListener('click', () => {
+  window.location.href = 'cadastro.html';
+});
+
+function mostrarClique() {
+  clique.style.opacity = '100';
+}
+criarConta.addEventListener('mouseover', mostrarClique);
+
+function esconderClique() {
+  clique.style.opacity = '0';
+}
+criarConta.addEventListener('mouseout', esconderClique);
+
+const entrar = document.querySelector('#entrei');
+const botao = document.querySelector('.entrar');
+botao.addEventListener('mouseover', () => {
+  entrar.style.opacity = '100';
+});
+botao.addEventListener('mouseout', () => {
+  entrar.style.opacity = '0';
 });
 
 const container = document.getElementById('container');
