81329ce677c63695f52e61400fa12a9048821662
Hyanka <hy.anka@hotmail.com>
2024-08-13 17:40:20 -0300

    atualizando

--- produtos.js
+++ produtos.js
@@ -1,23 +1,25 @@
-// Seleciona todos os elementos com a classe 'popup-trigger'
-const popupTriggers = document.querySelectorAll('.popup-trigger');
-const popupModal = document.getElementById('product-popup');
-const closePopup = document.querySelector('.close-popup');
+// Get the modal
+var modal = document.getElementById("product-modal");
 
-// Abre o pop-up quando clicar em qualquer imagem com a classe 'popup-trigger'
-popupTriggers.forEach(trigger => {
-    trigger.addEventListener('click', function() {
-        popupModal.style.display = 'block';
-    });
-});
+// Get the image that opens the modal
+var img = document.querySelector('.product-card img[alt="Imagem do Produto 1"]');
 
-// Fecha o pop-up quando o botão de fechar é clicado
-closePopup.addEventListener('click', function() {
-    popupModal.style.display = 'none';
-});
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-// Fecha o pop-up quando o usuário clica fora do conteúdo do pop-up
-window.addEventListener('click', function(event) {
-    if (event.target == popupModal) {
-        popupModal.style.display = 'none';
+// When the user clicks on the image, open the modal
+img.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
-});
+}
