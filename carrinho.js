179dfc0d06b9f5933bc05669b7e8e72559014b1e
vanessa <nessarbf@gmail.com>
2024-08-13 10:48:25 -0300

    alterações js

--- carrinho.js
+++
@@ -1,4 +0,0 @@
-sessionStorage.descricao = document.getElementById("descricao").innerHTML;
-sessionStorage.Valor = document.getElementById("valor").innerHTML;
-function resumoCompra() {
-    window.location = "resumo.html";
